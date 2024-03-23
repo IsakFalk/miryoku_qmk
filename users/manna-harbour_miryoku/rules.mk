b015b6e3fbe4e6ef1c406f7010de0d509bf43b98
Isak Falk <me@isakfalk.com>
2024-03-23 09:20:44 -0400

    Revert

--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -10,8 +10,6 @@ KEY_OVERRIDE_ENABLE = yes
 
 INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
-BOOTLOADER = rp2040
-
 include users/manna-harbour_miryoku/custom_rules.mk
 
 include users/manna-harbour_miryoku/post_rules.mk
